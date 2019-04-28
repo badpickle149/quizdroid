af4ce6c2ef3c54a043f37747adb79f0c614eb1dd
Kevin Weng <kevinweng95@gmail.com>
2019-04-28 15:49:19 -0700

    added initial styles

---
+++ app/src/main/java/edu/us/ischool/weng2k17/quizdroid/MainActivity.kt
@@ -0,0 +1,12 @@
+package edu.us.ischool.weng2k17.quizdroid
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
